037576ed83032450242683e8a8451988fda76257
Trunki Enqark <trunki.enqark@gmail.com>
2024-05-11 12:29:38 +0700

    update

--- src/information.rs
+++ src/information.rs
@@ -1,28 +1,23 @@
-use std::io;
+use std::{f32::consts::PI, io};
 use rand::Rng;
 
 // const INFI: usize = 1_000_000_000;
 const MAX_STUDENT_ID: usize = 999_999;
 
 #[derive(Debug, Clone)]
-pub enum GenderState { Male, Female, Sexless, Nothing }
+pub enum GenderState { Male, Female, Sexless }
 
 #[derive(Clone, Copy, Debug)]
-pub enum MajorState {
-    SE,
-    SS,
-    SA,
-    Nothing
-}
+pub enum MajorState { SE, SS, SA, Nothing }
 
 #[derive(Debug, Clone)]
 pub struct Birth { year: u16, month: u16, day: u16 }
 
 #[derive(Debug, Clone)]
 pub struct Student {
-    id: String,
     name: String,
     major: MajorState,
+    id: String,
     birthday: Birth,
     gender: GenderState,
     address: String,
@@ -31,15 +26,42 @@ pub struct Student {
 }
 
 impl Birth {
+    fn leap_year(yy: u16) -> bool {
+        yy % 4 == 0 && (yy % 100 != 0 || yy % 400 == 0)
+    }
+
     fn check_day(&self) -> bool {
-        let is_leap_year: bool = self.year % 4 == 0 && (self.year % 100 != 0 || self.year % 400 == 0);
+        let is_leap_year: bool = Birth::leap_year(self.year);
         if (1 <= self.day && self.day <= 31) && (self.month == 1 || self.month == 3 || self.month == 5 || self.month == 7 || self.month == 8 || self.month == 10 || self.month == 12) {
             return true;
         }
         if (1 <= self.day && self.day <= 30) && (self.month == 4 || self.month == 6 || self.month == 9 || self.month == 11) {
             return true;
         }
-        return (is_leap_year && self.day == 29) || (is_leap_year && self.day == 28);
+        return (is_leap_year && 1 <= self.day && self.day <= 29) || (!is_leap_year && 1 <= self.day && self.day <= 28);
+    }
+
+    fn birthday_maker() -> Birth {
+        let yy: u16 = rand::thread_rng().gen_range(1900..=2024);
+        let mm: u16 = rand::thread_rng().gen_range(1..=12);
+        let mut dd = 10;
+        if mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12 {
+            dd = rand::thread_rng().gen_range(1..=31);
+        } else if mm == 1 || mm == 3 || mm == 5 || mm == 7 || mm == 8 || mm == 10 || mm == 12 {
+            dd = rand::thread_rng().gen_range(1..=30);
+        } else if mm == 2 {
+            if Birth::leap_year(yy) {
+                dd = rand::thread_rng().gen_range(1..=29);
+            } else {
+                dd = rand::thread_rng().gen_range(1..=28);
+            }
+        }
+
+        Birth {
+            year: yy,
+            month: mm,
+            day: dd
+        }
     }
 
     pub fn enter_birthday() -> Birth {
@@ -98,9 +120,7 @@ impl Birth {
 
 impl Student {
     pub fn new_student() -> Student {
-        // println!("BUG!");
         let enter_name = Student::enter_name();
-        // println!("BUG!");
         let enter_birth = Birth::enter_birthday();
         let enter_major = Student::enter_major();
         let enter_gender = Student::enter_gender();
@@ -110,8 +130,8 @@ impl Student {
         let enter_id = Student::make_student_id(enter_major.clone());
         return Student {
             name: enter_name,
-            id: enter_id,
             major: enter_major,
+            id: enter_id,
             birthday: enter_birth,
             gender: enter_gender,
             address: enter_address,
@@ -124,11 +144,104 @@ impl Student {
         self.id.clone()
     }
 
-    // pub fn get_name(&self) -> String {
-    //     self.name.clone()
-    // }
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
 
-    pub fn enter_name() -> String {
+    pub fn get_major(&self) -> String {
+        match self.major {
+            MajorState::SA => "SA".to_string(),
+            MajorState::SE => "SE".to_string(),
+            MajorState::SS => "SS".to_string(),
+            MajorState::Nothing => "Nothing".to_string()
+        }
+    }
+
+    pub fn get_birthday(&self) -> String {
+        let mut dd = self.birthday.day.clone().to_string();
+        let mut mm = self.birthday.month.clone().to_string();
+        let yy = self.birthday.year.clone().to_string();
+        if dd.len() == 1 {
+            dd = "0".to_string() + &dd;
+        }
+        if mm.len() == 1 {
+            mm = "0".to_string() + &mm;
+        }
+        let result = dd + "/" + &mm + "/" + &yy;
+        result
+    }
+
+    pub fn get_gender(&self) -> String {
+        match self.gender {
+            GenderState::Male => "Male".to_string(),
+            GenderState::Female => "Female".to_string(),
+            GenderState::Sexless => "Sexless".to_string()
+        }
+    }
+
+    pub fn get_address(&self) -> String {
+        self.address.clone()
+    }
+
+    pub fn get_phone_number(&self) -> String {
+        self.phone_number.clone().to_string()
+    }
+
+    pub fn get_email(&self) -> String {
+        self.email.clone()
+    }
+
+    pub fn student_auto_maker() -> Student {
+        let major_temp = Student::major_random();
+
+        Student {
+            name: Student::string_random(),
+            major: major_temp,
+            id: Student::make_student_id(major_temp),
+            birthday: Birth::birthday_maker(),
+            gender: Student::gender_random(),
+            address: Student::string_random(),
+            phone_number: Student::phone_random(),
+            email: Student::string_random()
+        }
+    }
+
+    fn string_random() -> String {
+        let mut result = String::new();
+        for _ in 0..10 {
+            let number_random = rand::thread_rng().gen_range('a'..='z');
+            result += &number_random.clone().to_string();
+        }
+        result
+    }
+
+    fn major_random() -> MajorState {
+        match rand::thread_rng().gen_range(0..=2) {
+            0 => MajorState::SE,
+            1 => MajorState::SA,
+            2 => MajorState::SS,
+            _ => MajorState::Nothing
+        }
+    }
+
+    fn gender_random() -> GenderState {
+        match rand::thread_rng().gen_range(0..=1) {
+            0 => GenderState::Male,
+            1 => GenderState::Female,
+            _ => GenderState::Sexless,
+        }
+    }
+
+    fn phone_random() -> String {
+        let mut result = String::new();
+        for _ in 0..10 {
+            let number_random = rand::thread_rng().gen_range(0..=9);
+            result += &number_random.clone().to_string();
+        }
+        result
+    }
+
+    fn enter_name() -> String {
         let result = loop {
             let mut temp = String::new();
             println!("Enter your full name: ");
@@ -141,7 +254,7 @@ impl Student {
         return result;
     }
 
-    pub fn enter_major() -> MajorState {
+    fn enter_major() -> MajorState {
         println!("Major list: ");
         println!("1. SE");
         println!("2. SS");
@@ -166,7 +279,7 @@ impl Student {
         }
     }
 
-    pub fn enter_gender() -> GenderState {
+    fn enter_gender() -> GenderState {
         println!("Gender: ");
         println!("1. Male");
         println!("2. Female");
@@ -186,12 +299,11 @@ impl Student {
         match num { 
             1 => GenderState::Male,
             2 => GenderState::Female,
-            3 => GenderState::Sexless,
-            _ => GenderState::Nothing
+            _ => GenderState::Sexless,
         }
     }
 
-    pub fn enter_address() -> String {
+    fn enter_address() -> String {
         let mut temp = String::new();
         println!("Press your address: ");
         io::stdin().read_line(&mut temp).expect("Wrong input!");
@@ -199,7 +311,7 @@ impl Student {
         return temp;
     }
 
-    pub fn enter_phone_number() -> String {
+    fn enter_phone_number() -> String {
         let result = loop {
             let mut temp = String::new();
             println!("Enter your phone number: ");
@@ -212,7 +324,7 @@ impl Student {
         return result;
     }
 
-    pub fn enter_email() -> String {
+    fn enter_email() -> String {
         let mut temp = String::new();
         println!("Press your Email: ");
         io::stdin().read_line(&mut temp).expect("Wrong input!");
@@ -220,7 +332,7 @@ impl Student {
         temp
     }
 
-    pub fn make_student_id(major: MajorState) -> String {
+    fn make_student_id(major: MajorState) -> String {
         let maj = match major {
             MajorState::SA => "SA",
             MajorState::SE => "SE",
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-use std::io;
-// use crate::information::Student;
+use std::{fs::File, io::Write};
 
-use information::{Student, Birth};
+use information::Student;
 mod information;
 
 fn main() {
-    let new_stu = Student::new_student();
-    println!("{:?}", new_stu);
+    // let news = Student::student_auto_maker();
+    // println!("{:?}", news);
+    let mut w = File::create("./test.txt").unwrap();
+    for i in 0..100 {
+        let new_stu = Student::student_auto_maker();
+        writeln!(w, "{} | {} | {} | {} | {} | {} | {} | {}", 
+            new_stu.get_id(),
+            new_stu.get_name(),
+            new_stu.get_gender(),
+            new_stu.get_birthday(),
+            new_stu.get_phone_number(),
+            new_stu.get_email(),    
+            new_stu.get_major(),
+            new_stu.get_address()
+        ).unwrap();
+    }
 }
